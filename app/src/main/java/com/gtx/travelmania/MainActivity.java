35d6f19d3b9c902208f6fdbbfcf58e54ce97da40
domkatbess <domkatbess@gmail.com>
2019-08-03 17:22:56 +0100

    First commit-project set up

---
+++ app/src/main/java/com/gtx/travelmania/MainActivity.java
@@ -0,0 +1,13 @@
+package com.gtx.travelmania;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
